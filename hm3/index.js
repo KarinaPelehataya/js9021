2a3ae32973a6efdfdfd001fff87abe2a14923781
KarinaPelehataya <pelehatayak@gmail.com>
2021-10-05 20:16:53 +0300

    escape

--- hm3/index.js
+++ hm3/index.js
@@ -1,8 +1,4 @@
-//Task 1
-// Почитать здесь теорию по этому заданию 
-// https://medium.com/@alivander/%D1%81%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0-%D0%BF%D1%83%D0%B7%D1%8B%D1%80%D1%8C%D0%BA%D0%BE%D0%BC-javascript-54462b2989a6
-
-
+//Task 1 
 const bubbleSort = arr => {
     for (let i = 0, endI = arr.length - 1; i < endI; i++) {
         let wasSwap = false;
@@ -16,9 +12,22 @@ const bubbleSort = arr => {
     }
     return arr;
 };
-
 const result = bubbleSort([5, 8, 1, 9, 2, 6, 3, 7, 4]);
 console.log(result)
+
+//Task 1 in class
+  let arr = [5, 8, 1, 9, 2, 6, 3, 7, 4]
+  
+  for(let i=0; i<arr.length; i++) {
+    for(let j=1; j=<arr.length-1; j++) {
+        if(aee[j]>arr[j+1]){
+            let min= arr[j+1];
+            arr[j+1]= arr[j];
+            arr[j]=min;
+        }
+    }
+  }
+
 
 //Task 2
 
@@ -34,15 +43,37 @@ function ChessAndRise(risePrice, riseItemWeight) {
     return allRisePrice
 }
 
+// Task 2 in class
+let arruza= 0.000019;
+let price =1;// usd
+let rice_count = 1;// short version  rc=1+ Math.pow(2,63);
+let weight;
+for(let i=0;i<64; i++){
+    rice_weight = additionalCount * arruza;
+}
+console.log(rice_weight)
+
 //Task 3
-//За это задание хз нужно посмотреть
-// Инфа тут
-//https://learn.javascript.ru/task/fibonacci-numbers
-
-
 function fib(n) {
     return n <= 1 ? n : fib(n - 1) + fib(n - 2);
 }
 
+// Task 3 in class
 
-ChessAndRise(1, 0.019)
+let fib=11;
+let a= 1;
+let b= 1;
+let c;
+
+let fibNum; 
+for(i =2;i<= fib; i++){
+    c=a+b;
+    a=b;
+    b=c;
+}
+console.log(fibNum);
+
+let fibArr=[1,1];
+for(let i=2; i<= fib;i++){
+    fibArr[i];
+}
